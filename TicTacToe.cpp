4e08a685ccb1aba667ae25b82c340998247d7f41
ungpung0 <ungpung0@gmail.com>
2022-11-10 23:59:36 +0900

    Commit main.cpp
    파일 인코딩 변경.
    프로그램 버그 수정.

---
+++ TicTacToe.cpp
@@ -0,0 +1,233 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "TicTacToe.h"
+
+enum TicTacToe::commands {
+    COMMAND_HELP = 1,
+    COMMAND_PLAY,
+    COMMAND_STAT,
+    COMMAND_REPLAY,
+    COMMAND_EXIT
+};
+
+void TicTacToe::showHelp() {
+    system("cls");
+    std::cout << "[커맨드 설명]\n" << std::endl;
+    std::cout << "1 :: 커맨드에 대한 설명을 보여줍니다.\n" << std::endl;
+    std::cout << "2 :: 행과 열을 입력하여 게임을 진행합니다.\n" << std::endl;
+    std::cout << "3 :: 현재 게임의 상황을 출력합니다.\n" << std::endl;
+    std::cout << "4 :: 현재 게임을 초기화합니다.\n" << std::endl;
+    std::cout << "5 :: 틱택토 게임을 종료합니다." << std::endl;
+}
+
+bool* TicTacToe::checkWinn(int* sumOne, int* sumTwo) {
+    if (abs(*sumOne) == 3 || abs(*sumTwo) == 3)
+        return (bool*) false;
+    else
+        return (bool*) true;
+}
+
+bool* TicTacToe::checkGame(int board[][LENGTH], bool* condition, int* col, int* row) {
+    int sumOne = 0;
+    int sumTwo = 0;
+
+    // 가로/세로 체크.
+    for (int i = 0; i < LENGTH; i++) {
+        sumOne += board[*col][i];
+        sumTwo += board[i][*row];
+        if (abs(sumOne) == 3 || abs(sumTwo) == 3)
+            *condition = false;
+    }
+    if (*condition == false) {
+        return condition;
+    }
+
+    // 정대각/역대각 체크.
+    if ((*col + *row) % 2 == 0) {
+        sumOne = 0, sumTwo = 0;
+        for (int i = 0; i < LENGTH; i++) {
+            sumOne += board[i][i];
+            sumTwo += board[i][(LENGTH - 1) - i];
+            if (abs(sumOne) == 3 || abs(sumTwo) == 3)
+                *condition = false;
+        }
+    }
+
+    return condition;
+}
+
+void TicTacToe::showStatus(int board[][LENGTH], bool* condition, char* player) {
+    system("cls");
+    std::cout << "[현재 보드판]" << std::endl;
+    std::cout << "   [0][1][2]" << std::endl;
+
+    for (int i = 0; i < LENGTH; i++) {
+        std::cout << "[" << i << "]";
+        for (int j : board[i]) {
+            if (j == 1)
+                std::cout << "[O]";
+            else if (j == -1)
+                std::cout << "[X]";
+            else
+                std::cout << "[ ]";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "\n[게임 상태]" << std::endl;
+
+    if (*condition == true) {
+        std::cout << "게임이 아직 진행중입니다." << std::endl;
+    }
+    else {
+        if (*player == '\0')
+            std::cout << "게임이 무승부로 끝났습니다." << std::endl;
+        else
+            std::cout << *player << " 의 승리로 게임이 끝났습니다.";
+    }
+}
+
+char* TicTacToe::playGame(int board[][LENGTH], bool* condition, char* player, int* turn) {
+    system("cls");
+    TicTacToe::showStatus(board, condition, player);
+
+    if (*turn == 0) {
+        while (true) {
+            std::cout << "\n선공 플레이어를 입력해주세요. ('O', 'X')" << std::endl;
+            std::cout << "플레이어 > ";
+            std::cin >> player;
+
+            if (*player == 'O' || *player == 'X') {
+                std::cout << *player << " 의 선공으로 게임을 시작합니다." << std::endl;
+                *turn = *turn + 1;
+                *condition = true;
+                break;
+            }
+            else {
+                std::cout << "잘못된 플레이어를 입력했습니다." << std::endl;
+            }
+        }
+    }
+
+    if (*condition == true) {
+        std::cout << "\n" << *player << " 의 차례입니다." << std::endl;
+
+        while (true) {
+            int col = -1, row = -1;
+            std::cout << "행과 열을 입력하세요." << std::endl;
+            std::cout << "행 > ";
+            std::cin >> col;
+            std::cout << "열 > ";
+            std::cin >> row;
+
+            if (col >= LENGTH || row >= LENGTH) {
+                std::cout << "잘못된 인덱스 입니다." << std::endl;
+                continue;
+            }
+            else if (board[col][row] != 0) {
+                std::cout << "점유된 인덱스 입니다." << std::endl;
+                continue;
+            }
+            else {
+                *turn = *turn + 1;
+                board[col][row] = (*player == 'O') ? 1 : -1;
+                condition = TicTacToe::checkGame(board, condition, &col, &row);
+                *player = (*player == 'O' && *condition == true) ? 'X' : 'O';
+                
+                if (*turn == MAX) {
+                    *condition = false;
+                    *player = '\0';
+                }
+                break;
+            }
+        }
+    }
+    else {
+        std::cout << " 게임을 재시작 하세요." << std::endl;
+    }
+
+    return player;
+}
+
+
+
+void TicTacToe::replayGame(int board[][LENGTH], bool* condition, char* player, int* turn) {
+    system("cls");
+    std::cout << "[게임 초기화]" << std::endl;
+    std::cout << "게임을 초기화하겠습니까? (Y, N)" << std::endl;
+
+    while (true) {
+        std::cout << "입력 > ";
+        char answer = '\0';
+        std::cin >> answer;
+
+        if (answer == 'y' || answer == 'Y') {
+            for (int i = 0; i < LENGTH; i++) {
+                for (int j = 0; j < LENGTH; j++) {
+                    board[i][j] = 0;
+                }
+            }
+
+            *player = '\0';
+            *turn = 0;
+            *condition = true;
+
+            std::cout << "초기화가 끝났습니다." << std::endl;
+            break;
+        }
+        else if (answer == 'n' || answer == 'N') {
+            std::cout << "초기화하지 않습니다." << std::endl;
+            break;
+        }
+        else {
+            std::cout << "잘못된 문자 입니다. 다시 입력하세요.\n" << std::endl;
+            continue;
+        }
+    }
+}
+
+
+void TicTacToe::exitGame() {
+    system("cls");
+    std::cout << "게임을 종료합니다. 재미있게 즐기셨나요? ;)";
+}
+
+void TicTacToe::showMenu() {
+    system("cls");
+    std::cout << "=============================================" << std::endl;
+    std::cout << "//           LETS PLAY TICTACTOE!          //" << std::endl;
+    std::cout << "//           Created By juwon              //" << std::endl;
+    std::cout << "//                      Input '1' to HELP. //" << std::endl;
+    std::cout << "=============================================" << std::endl;
+
+    while (true) {
+        int command;
+        std::cout << "\n커맨드 > ";
+        std::cin >> command;
+
+        if (command == COMMAND_HELP) {
+            TicTacToe::showHelp();
+            continue;
+        }
+        else if (command == COMMAND_PLAY) {
+            TicTacToe::playGame(board, &condition, &player, &turn);
+            continue;
+        }
+        else if (command == COMMAND_STAT) {
+            TicTacToe::showStatus(board, &condition, &player);
+            continue;
+        }
+        else if (command == COMMAND_REPLAY) {
+            TicTacToe::replayGame(board, &condition, &player, &turn);
+            continue;
+        }
+        else if (command == COMMAND_EXIT) {
+            TicTacToe::exitGame();
+            break;
+        }
+        else {
+            std::cout << "잘못된 번호 입니다. 다시 입력하세요." << std::endl;
+            continue;
+        }
+    }
+}
---
+++ TicTacToe.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#define LENGTH 3
+#define MAX 10
+
+class TicTacToe {
+    /*
+        board     : (O, X)를 각각 (1, -1)로 대응하여 저장하는 2차원 배열.
+        turn      : 현재 턴을 저장하는 int 변수.
+        player    : 현재 플레이어를 저장하는 char 변수.
+        condition : 게임의 상태를 저장하는 bool 변수.
+    */
+    int board[LENGTH][LENGTH] = { 0 };
+    int turn = 0;
+    char player = '\0';
+    bool condition = true;
+
+public:
+    /*
+        [함수 설명]
+        commands     : showMenu()에서 사용할 열거형 자료형.
+        showMenu()   : 커맨드를 입력하는 메뉴를 호출하는 함수.
+        showHelp()   : 프로그램에서 커맨드의 설명을 출력하는 함수.
+        showStatus() : 게임의 현재 보드판, 상태를 출력하는 함수.
+        checkGame()  : 게임의 승리를 검사하여 bool*로 반환하는 함수.
+        playGame()   : 행과 열을 입력하여 게임을 진행하는 함수.
+        replayGame() : 게임을 초기화하는 함수.
+        exitGame()   : 게임을 종료하는 함수.
+    */
+    enum commands;
+    void showMenu();
+    void showHelp();
+    void showStatus(int board[][LENGTH], bool* condition, char* player);
+    bool* checkWinn(int* sumOne, int* sumTwo);
+    bool* checkGame(int board[][LENGTH], bool* condition, int* col, int* row);
+    char* playGame(int board[][LENGTH], bool* condition, char* player, int* turn);
+    void replayGame(int board[][LENGTH], bool* condition, char* player, int* turn);
+    void exitGame();
+};
